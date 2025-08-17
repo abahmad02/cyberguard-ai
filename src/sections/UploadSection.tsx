ddaf3f6c20d767bd9c37881e2007a35de64754e6
AlkylonSU <abdullah.ahmad@sabanciuniv.edu>
2025-08-17 20:42:33 +0500

    Added URL

--- src/sections/UploadSection.tsx
+++ src/sections/UploadSection.tsx
@@ -124,7 +124,7 @@ interface ScanResult {
   status: string;
 }
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
+const API_BASE_URL = 'https://CyberAI.pythonanywhere.com/api';
 
 const UploadSection: React.FC = () => {
   const [isDragOver, setIsDragOver] = useState(false);
